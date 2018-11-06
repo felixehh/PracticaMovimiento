d63e0f987420a8aeedb4d7c3b70ef9363773f269
ricky felix <ricky_felix@hotmail.com>
2018-11-06 08:32:20 -0700

    threads

--- PracticaMovimiento/MainWindow.xaml.cs
+++ PracticaMovimiento/MainWindow.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Threading;
+using System.Diagnostics;
 
 namespace PracticaMovimiento
 {
@@ -20,10 +22,46 @@ namespace PracticaMovimiento
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        Stopwatch stopwatch;
+        TimeSpan tiempoAnterior;
+
         public MainWindow()
         {
             InitializeComponent();
             miCanvas.Focus();
+
+            stopwatch = new Stopwatch();
+            stopwatch.Start();
+            tiempoAnterior = stopwatch.Elapsed;
+
+            //1. establecer instrucciones
+            ThreadStart threadStart = new ThreadStart(moverEnemigos);
+
+            Thread threadMoverEnemigos = new Thread(threadStart);
+
+            threadMoverEnemigos.Start();
+        }
+
+        void moverEnemigos()
+        {
+            while (true)
+            {
+                Dispatcher.Invoke(
+                () =>
+                {
+                    var tiempoActual = stopwatch.Elapsed;
+                    var deltaTime = tiempoActual - tiempoAnterior;
+                    double leftCarroActual = Canvas.GetLeft(imgCarro);
+                    Canvas.SetLeft(imgCarro, leftCarroActual - (90 * deltaTime.TotalSeconds));
+                    if(Canvas.GetLeft(imgCarro) <= - 100)
+                    {
+                        Canvas.SetLeft(imgCarro, 800);
+                    }
+                    tiempoAnterior = tiempoActual;
+                }
+                );
+            }
         }
 
         private void miCanvas_KeyDown(object sender, KeyEventArgs e)
@@ -32,6 +70,11 @@ namespace PracticaMovimiento
             {
                 double topFatherActual = Canvas.GetTop(imgFather);
                 Canvas.SetTop(imgFather, topFatherActual - 15);
+            }
+            if (e.Key == Key.Left)
+            {
+                double leftFatherActual = Canvas.GetLeft(imgFather);
+                Canvas.SetLeft(imgFather, leftFatherActual - 15);
             }
         }
     }
